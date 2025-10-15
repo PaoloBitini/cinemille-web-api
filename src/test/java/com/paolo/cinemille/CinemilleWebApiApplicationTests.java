03f46647b7a0b4faa0379fd19531e47f1db8b652
PaoloBitini <44835229+PaoloBitini@users.noreply.github.com>
2025-10-15 17:27:21 +0200

    first commit

---
+++ src/test/java/com/paolo/cinemille/CinemilleWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paolo.cinemille;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemilleWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
